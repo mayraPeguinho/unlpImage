b3a40050318578c355a8738fca744aed16cfa2bf
GABRIELA BAMBA BALDIVIESO <gabrielabamba123@gmail.com>
2023-04-29 20:31:42 -0300

    Agrego interfaz de generador de memes

---
+++ pantallas/generador_memes.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+
+def generar_meme():
+    layout = [
+             [sg.Text('Seleccionar imagen:')],
+             [sg.Input(), sg.FolderBrowse(button_text= "Seleccionar imagen")],
+             [sg.Text('Introducir el texto:')],
+             [sg.InputText()],
+             [sg.OK(button_text="Generar"), sg.Cancel(button_text="Volver")]
+            
+           ]
+    window = sg.Window('Generador de memes').Layout(layout)
+
+    while True:
+         evento, valores = window.Read()
+    
+         if evento == 'Generar':
+             pass   
+         elif evento == 'Volver' or evento== sg.WIN_CLOSED:
+             break
+        
+    window.Close()
+
+generar_meme()
+  
