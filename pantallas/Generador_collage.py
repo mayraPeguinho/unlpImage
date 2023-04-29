3b1c0ba77d58b546f719335c75afc4413750ee88
GABRIELA BAMBA BALDIVIESO <gabrielabamba123@gmail.com>
2023-04-29 20:37:07 -0300

    Agrego interfaz de generador de collage

---
+++ pantallas/Generador_collage.py
@@ -0,0 +1,22 @@
+import PySimpleGUI as sg
+
+layout = [
+   
+    [sg.Text("Seleccionar imágenes:"),sg.Input(), sg.FileBrowse(button_text= "Seleccionar")],
+    [sg.Text("Seleccionar plantilla:   "),sg.Combo(["Plantilla 1", "Plantilla 2", "Plantilla 3","Plantilla 4"],key="Listar Plantillas")],
+    [sg.Button("Generar collage"), sg.Button("Salir")]
+]
+
+window = sg.Window('Generador de collage', layout, margins=(200,150))
+
+while True:
+    evento, valores = window.read()
+    if evento == sg.WIN_CLOSED or evento == "Salir":
+        break
+    elif evento == "Generar collage":
+        #funcion que genera
+        print("Collage generado correctamente")
+window.close()
+
+
+    
