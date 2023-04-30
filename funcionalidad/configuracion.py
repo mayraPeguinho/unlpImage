7a1d0d4c0c3e23ac3f009b07634cc03eaacab4f5
EZEQUIEL PALLINI <ezequielpallini2@gmail.com>
2023-04-30 18:54:31 -0300

    Agregada funcionalidad a la pantalla de configuración

---
+++ funcionalidad/configuracion.py
@@ -0,0 +1,15 @@
+import json
+import sys
+import os
+
+#Agrego el directorio raiz a la ruta de búsqueda de módulos
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+def guardar_directorios(directorio_imagenes, directorio_collages, directorio_memes):
+    data = {'directorio_imagenes': directorio_imagenes,
+            'directorio_collages': directorio_collages,
+            'directorio_memes': directorio_memes}
+    with open(os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'datos')), 'configuracion.json'), 'w') as archivo:
+        json.dump(data, archivo)
+        archivo.close()
--- pantallas/configuracion.py
+++ pantallas/configuracion.py
@@ -1,29 +1,33 @@
 import PySimpleGUI as sg
-#from funcionalidad import congifuracion as cf 
+import sys
+import os
 
+# Agrego el directorio raiz a la ruta de búsqueda de módulos
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-#layout = [[sg.Input('Repositorio de imagenes')], [sg.Input('Directorio de collages')], 
-#          [sg.Input('Repositorio de imagenes')]]
-
-#sg.Window("Configuración", layout, margins=(200, 150)).read()
+from funcionalidad.configuracion import guardar_directorios
 
 layout = [[sg.Text('Repositorio de imagenes')],
+          #En este directorio se encuentran las imagenes que podemos utilizar en la aplicación
           [sg.Input(), sg.FolderBrowse(button_text= "Seleccionar")],
           [sg.Text('Directorio de collages')],
+          #En este directorio almacenaremos los collages
           [sg.Input(), sg.FolderBrowse(button_text= "Seleccionar")],
           [sg.Text('Directorio de memes')],
+          #En este directorio almacenaremos los memes
           [sg.Input(), sg.FolderBrowse(button_text= "Seleccionar")],
+          #Botones de guardar y volver
           [sg.OK(button_text="Guardar"), sg.Cancel(button_text="Volver")]]
 
-window = sg.Window("Configuración", layout, margins=(200, 150)).read()
-
+window = sg.Window("Configuración", layout, margins=(200, 150))
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
+    if event == sg.WIN_CLOSED or event == 'Volver':
         break
-    if event == 'OK':
-        folder_path = values[0]
-        print(f'Se seleccionó la carpeta: {folder_path}')
-
+    if event == 'Guardar':
+        repo_imagenes = values[0]
+        carpeta_collages = values[1]
+        carpeta_memes = values[2]
+        guardar_directorios(repo_imagenes, carpeta_collages, carpeta_memes)
 window.close()
