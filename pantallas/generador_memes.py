c6e1d7eeb9fa065b72523c36892d22411404c6ee
Gabriela Bamba Baldivieso <gabrielabamba123@gmail.com>
2023-05-06 21:49:20 -0300

    Modifico la ventana generador de memes

--- pantallas/generador_memes.py
+++ pantallas/generador_memes.py
@@ -1,22 +1,26 @@
 import PySimpleGUI as sg
 
 def generar_meme():
+    '''Esta funcion define una ventana 
+       para generar memes a partir de una imágen y un seleccionados por el usuario. '''
     layout = [
+             [ sg.Text("Generar Meme", font=("Helvetica", 20), justification="left"),
+             sg.Column([[sg.Button("Volver ➡", key="-VOLVER-")]],expand_x=True,element_justification="right"),],
              [sg.Text('Seleccionar imagen:')],
              [sg.Input(), sg.FolderBrowse(button_text= "Seleccionar imagen")],
              [sg.Text('Introducir el texto:')],
              [sg.InputText()],
-             [sg.OK(button_text="Generar"), sg.Cancel(button_text="Volver")]
+             [sg.Column([[sg.Button("Generar", "-GENERAR-")]], expand_x=True, element_justification="right")],
             
            ]
-    window = sg.Window('Generador de memes').Layout(layout)
+    window = sg.Window('Generador de memes',layout, margins=(60, 80), finalize=True, resizable=True)
 
     while True:
          evento, valores = window.Read()
     
-         if evento == 'Generar':
+         if evento == "-GENERAR-":
              pass   
-         elif evento == 'Volver' or evento== sg.WIN_CLOSED:
+         elif evento == "-VOLVER-" or evento== sg.WIN_CLOSED:
              break
         
     window.Close()
