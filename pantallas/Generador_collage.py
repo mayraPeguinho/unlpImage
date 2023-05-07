e454e3982c1f988803d02091c8a9375787256b40
Gabriela Bamba Baldivieso <gabrielabamba123@gmail.com>
2023-05-06 21:09:25 -0300

    Modifico la ventana generador de collages

--- pantallas/Generador_collage.py
+++ pantallas/Generador_collage.py
@@ -1,25 +1,39 @@
 import PySimpleGUI as sg
 
 def generar_collage():
-    layout = [
-             [sg.Text("Seleccionar imágenes:"),sg.Input(), sg.FileBrowse(button_text= "Seleccionar")],
-             [sg.Text("Seleccionar plantilla:   "),sg.Combo(["Plantilla 1", "Plantilla 2", "Plantilla 3","Plantilla 4"],key="Listar Plantillas")],
-             [sg.Button("Generar collage"), sg.Button("Salir")]
-             ]
+     '''Esta funcion define una ventana 
+       para generar collages a partir de imágenes y plantillas seleccionadas por el usuario. '''
+     layout = [
+    [
+        sg.Text("Generar Collage", font=("Helvetica", 20), justification="left"),
+        sg.Column(
+            [[sg.Button("Volver ➡", key="-VOLVER-")]],
+            expand_x=True,
+            element_justification="right"
+        ),
+     ],
+     [sg.Text("Seleccionar imágenes")],
+     [sg.Input(), sg.FileBrowse(button_text="Seleccionar")],
+     [sg.Text("Seleccionar plantilla")],
+     [sg.Combo(["Plantilla 1", "Plantilla 2", "Plantilla 3", "Plantilla 4"], key="Listar Plantillas")],
+     [sg.Column([[sg.Button("Generar Collage", "-GENERAR COLLAGE-")]], expand_x=True, element_justification="right")],
+     ]
 
-    window = sg.Window('Generador de collage', layout, margins=(200,150))
+     window = sg.Window('Generador de collage', layout, margins=(60, 80), finalize=True, resizable=True)
 
-    while True:
-        evento, valores = window.read()
-        if evento == sg.WIN_CLOSED or evento == "Salir":
-            break
-        elif evento == "Generar collage":
-            #funcion que genera
-            print("Collage generado correctamente")
-    window.close()
+     while True:
+         evento, valores = window.read()
+         if evento == sg.WIN_CLOSED or evento == "-VOLVER-":
+             break
+         elif evento == "-GENERAR COLLAGE-":
+              # función que genera
+              print("Collage generado correctamente")
+
+     window.close()
+
 
 if __name__ =="__main__":
-    generar_collage()
+     generar_collage()
 
 
     
