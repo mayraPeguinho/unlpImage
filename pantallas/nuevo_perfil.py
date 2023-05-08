99d278c9f5baf382dd2b0b5be19d7e78e1e5f261
Francisco Garrubba <franciscogarrubba@gmail.com>
2023-05-08 00:51:04 -0300

    Agregue para hacer un archivo json en nuevo_perfil, algunas modificaciones en la carpeta imagenes

--- pantallas/nuevo_perfil.py
+++ pantallas/nuevo_perfil.py
@@ -4,31 +4,31 @@ import json
 import PySimpleGUI as sg
 from PIL import Image
 
-ruta_imagen = os.path.join(os.getcwd(),'imagenes','imagenes_perfil', 'icon.png')
+ruta_imagen = os.path.join(os.getcwd(),'imagenes','imagenes_perfil', 'avatar.png')
 
 ruta_archivo = os.path.join(os.getcwd(),'datos', 'perfil_nuevo.json')
 
 columna_izquierda = [[sg.Text('Nuevo perfil')],
-          [sg.Text('Usuario:')],[sg.InputText(key='-USUARIO-')], 
+          [sg.Text('Usuario:')],[sg.InputText(key='-USUARIO-'), sg.Button('Validar', key='-VALIDAR-')], 
           [sg.Text('Nombre:')],[sg.InputText(key='-NOMBRE-')],
           [sg.Text('Edad:')],[sg.InputText(key='-EDAD-')],
           [sg.Text('Genero:')],
-          [sg.Listbox(['Masculino', 'Femenino', 'Otro'],no_scrollbar=False, s=(15,3))],
+          [sg.Listbox(['Masculino', 'Femenino', 'Otro'],no_scrollbar=False, s=(15,3),key='-GENERO-')],
              [sg.InputText(key='-ESPECIFICAR_GENERO-')],
           [sg.Button('Guardar', key='-GUARDAR-'), sg.Button('Volver', key='-VOLVER-')]]
 
 
 
 columna_derecha =   [
-                        [
+                    [
                         sg.Image(
                             source=ruta_imagen,
                             key=('-AVATAR-'),
                             size=(60,60),
                             pad=((125,125), (0,0)),
                         )
-                        ],
-                        [
+                    ],
+                    [
                         sg.FileBrowse(
                             "Seleccionar Imagen",
                             key=('-BROWSE-'),
@@ -36,9 +36,9 @@ columna_derecha =   [
                             change_submits=True,
                             size=(20,2),
                         ),   
-                        ],
+                    ],
                     ]
-       
+
 layout = [[sg.Column(columna_izquierda, element_justification='c'), sg.VSeperator(),sg.Column(columna_derecha, element_justification='c')]]
 
 window = sg.Window('Nuevo perfil', layout)
@@ -54,6 +54,13 @@ while True:
            source=filename,
            size=(60,60),
         )
+    elif event == '-VALIDAR-':
+        with open(ruta_archivo, 'r') as archivo:
+            datos_perfil = json.load(archivo)
+        mostrar_datos = json.dumps(datos_perfil, indent=4)
+        print(mostrar_datos)
+        #mostrar_datos[0] == '-USUARIO-':
+        #   sg.popup('Usuario existente, ingrese otro usuario')
     elif event == '-GUARDAR-':      
        with open(ruta_archivo, 'a') as archivo:
            json.dump(values, archivo)
