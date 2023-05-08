1d7300d6f51d79faa49fc2a2adf18318c83e9280
EZEQUIEL PALLINI <ezequielpallini2@gmail.com>
2023-05-08 13:31:27 -0300

    Corregidos errores, implementadas funcionalidades

---
+++ funcionalidad/etiquetar_imagenes.py
@@ -0,0 +1,90 @@
+import sys
+import os
+import json
+import PIL
+from PIL import Image, ImageTk
+from datetime import datetime
+import mimetypes
+import csv
+
+
+def imagen_tostring(datos):
+    """Devuelve un string con la descripción de la imagen."""
+
+    str = (" | {} | {} MB | {} | \nTags: {} \nDescripción: {}".format(datos[4], datos[5], datos[3], datos[2], datos[1]))
+
+    return str
+
+
+
+
+def guardar_data(ruta, data):
+    """Guarda los datos de la imagen en el archivo csv"""
+    ruta
+    with open(ruta, mode='r+') as file:
+            # Creo objeto lector
+            reader = csv.reader(file)
+            contenido_csv = list(reader)
+
+            encontre = False
+            #Busco la fila en el csv
+            for pos, datos_fila in enumerate(contenido_csv):
+                if data[0] in datos_fila:
+                    # me guardo la posicion en el archivo
+                    posicion = pos
+                    datos_previos_modificar = datos_fila
+                    encontre = True
+                    break
+            if encontre:
+                datos_imagen = datos_previos_modificar[:]
+                datos_imagen[1] = data[1]
+                datos_imagen[2] = data[2]
+                datos_imagen[6] = data[6]
+                datos_imagen[7] = "null" #De momento es null porque falta implementar funcionalidad
+                                        #de agregar ultimo perfil que actualizó
+                #Modifico el csv con los datos modificados de imagen existente
+                contenido_csv[posicion] = datos_imagen
+            else:
+                # Si no se encontró una fila con la ruta dada, agreo una nueva fila (nueva imagen editada)
+                fila_nueva = (data[0], data[1], data[2],data[3], data[4], data[5], data[6], "null")
+                #Modifico el csv con los datos agregados de una imagen nueva
+                contenido_csv.append(fila_nueva)
+
+            with open(ruta, 'w',newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(contenido_csv)
+    
+
+
+def traer_data(values):
+    """Retorna y actualiza los valores de la imagen que deben mostrarse y/o editarse."""
+    # traigo la ruta de la imagen
+    ruta_imagen = values['-TREE-'][0]
+    # la abro
+    imagen = Image.open(ruta_imagen)
+    #Extraigo la resolución antes de cambiarla
+    resolucion = imagen.size
+    descripcion = values['Texto']
+    #lista de tags(lo saco de imagen_seleccionada)
+    tags = values['Tag']
+    #tipo (mimetype)
+    mimetype = mimetypes.guess_type(ruta_imagen)[0]
+    #tamaño (metadata)
+    tamaño = os.path.getsize(ruta_imagen)
+    #Fecha de ultima actualización (enviar al log)
+    timestamp = datetime.timestamp(datetime.now())
+    ultima_actualizacion = datetime.fromtimestamp(timestamp)  
+    #----------------------------------------------------------------
+    #ultimo perfil que actualizó ---(/!\ ver con Fran como implementar esta funcionalidad)---
+    #----------------------------------------------------------------
+    datos = (ruta_imagen, descripcion, tags,(str(resolucion[0]) + 'x' + str(resolucion[1])), 
+                    mimetype, (round(tamaño / (1024*1024), 2)), ultima_actualizacion, "null")
+    return datos
+
+def mostrar_imagen(ruta):
+    imagen = Image.open(ruta)
+    #Modifico el tamaño para mostrarla en la pantalla
+    imagen = imagen.resize((350, 300))
+    # convertir la imagen a un formato que pueda mostrar PySimpleGUI
+    data = ImageTk.PhotoImage(imagen)
+    return data
--- pantallas/etiquetar_imagenes.py
+++ pantallas/etiquetar_imagenes.py
@@ -4,24 +4,20 @@ import os
 import json
 import PySimpleGUI as sg
 import PIL
-from PIL import Image, ImageTk
-from datetime import datetime
-import mimetypes
 
-import csv
+
 folder_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABnUlEQVQ4y8WSv2rUQRSFv7vZgJFFsQg2EkWb4AvEJ8hqKVilSmFn3iNvIAp21oIW9haihBRKiqwElMVsIJjNrprsOr/5dyzml3UhEQIWHhjmcpn7zblw4B9lJ8Xag9mlmQb3AJzX3tOX8Tngzg349q7t5xcfzpKGhOFHnjx+9qLTzW8wsmFTL2Gzk7Y2O/k9kCbtwUZbV+Zvo8Md3PALrjoiqsKSR9ljpAJpwOsNtlfXfRvoNU8Arr/NsVo0ry5z4dZN5hoGqEzYDChBOoKwS/vSq0XW3y5NAI/uN1cvLqzQur4MCpBGEEd1PQDfQ74HYR+LfeQOAOYAmgAmbly+dgfid5CHPIKqC74L8RDyGPIYy7+QQjFWa7ICsQ8SpB/IfcJSDVMAJUwJkYDMNOEPIBxA/gnuMyYPijXAI3lMse7FGnIKsIuqrxgRSeXOoYZUCI8pIKW/OHA7kD2YYcpAKgM5ABXk4qSsdJaDOMCsgTIYAlL5TQFTyUIZDmev0N/bnwqnylEBQS45UKnHx/lUlFvA3fo+jwR8ALb47/oNma38cuqiJ9AAAAAASUVORK5CYII='
 file_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABU0lEQVQ4y52TzStEURiHn/ecc6XG54JSdlMkNhYWsiILS0lsJaUsLW2Mv8CfIDtr2VtbY4GUEvmIZnKbZsY977Uwt2HcyW1+dTZvt6fn9557BGB+aaNQKBR2ifkbgWR+cX13ubO1svz++niVTA1ArDHDg91UahHFsMxbKWycYsjze4muTsP64vT43v7hSf/A0FgdjQPQWAmco68nB+T+SFSqNUQgcIbN1bn8Z3RwvL22MAvcu8TACFgrpMVZ4aUYcn77BMDkxGgemAGOHIBXxRjBWZMKoCPA2h6qEUSRR2MF6GxUUMUaIUgBCNTnAcm3H2G5YQfgvccYIXAtDH7FoKq/AaqKlbrBj2trFVXfBPAea4SOIIsBeN9kkCwxsNkAqRWy7+B7Z00G3xVc2wZeMSI4S7sVYkSk5Z/4PyBWROqvox3A28PN2cjUwinQC9QyckKALxj4kv2auK0xAAAAAElFTkSuQmCC'
 
+# Agrego el directorio raiz a la ruta de búsqueda de módulos
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from funcionalidad import etiquetar_imagenes
 
-
+#Leo desde el archivo de configuración, donde ir a buscar mi repositorio de imagenes
 ruta_archivo = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'datos')), 'configuracion.json')
-
-
-#!!!!!!!!!!!!!se va el tryexcept, trabajar con valores por defecto 
-
 with open(ruta_archivo) as f:
-    data = json.load(f)
-    starting_path = data['repositorio_imagenes']
+    datos = json.load(f)
+    starting_path = datos['repositorio_imagenes']
 
 #para poder mostrar los archivos en forma de cascada hay que usar un objeto "treedata" incluído en PySimplegui
 #lo extraje de la documentación oficinal de sg
@@ -56,9 +52,9 @@ columna_izquierda = [[sg.Text('Repositorio de imagenes')],
                    expand_y=True,
                    ),],
           [sg.Text('Tag:')], 
-          [sg.InputText(key='Tag'),sg.Button('Agregar')],
+          [sg.InputText(key='Tag'),sg.Button('Modificar')],
           [sg.Text('Texto descriptivo:')], 
-          [sg.InputText(key='Texto'),sg.Button('Agregar')],
+          [sg.InputText(key='Texto'),sg.Button('Modificar')],
           [sg.Button('Guardar'), sg.Button('Volver')]]
 
 
@@ -75,9 +71,6 @@ layout = [[sg.Column(columna_izquierda, element_justification='c'), sg.VSeperato
 #agregar en window el manejo de usuarios entre ventanas. 
 window = sg.Window('Etiquetar Imagenes', layout, resizable=False, finalize=True)
 
-imagen_seleccionada = {'ruta': '', 'tag': '', 'descripcion': ''}
-ruta_archivo_imagenes = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'datos')), 'etiquetas_imagenes.json')
-
 
 while True:     # Loop de eventos
     event, values = window.read()    
@@ -85,96 +78,32 @@ while True:     # Loop de eventos
         break
     else:
         if event == '-TREE-':
-            # Obtener la ruta de la imagen seleccionada
-            ruta_imagen = values['-TREE-'][0]
-            # Actualizar la variable imagen_seleccionada
-            imagen_seleccionada['ruta'] = ruta_imagen
-            # Cargar y mostrar la imagen en la columna de la derecha
-            if ruta_imagen.endswith(('.png', '.jpg', '.jpeg', '.gif')):
-                imagen = sg.Image(filename=ruta_imagen)
-                window['-IMAGE-'].update(imagen)
-                # Actualizar la etiqueta de texto de la imagen seleccionada en la columna de la derecha
-                window['-TOUT-'].update(os.path.basename(ruta_imagen))
-
-             #Chequear que se pueda abrir la imagen
+           
+            #Chequear que se pueda abrir y tratar la imagen
             try:
-                imagen = Image.open(os.path.join(ruta_imagen))
-                ruta_imagen = values['-TREE-'][0]
-                imagen = Image.open(ruta_imagen)
-                #Extraigo la resolución antes de cambiarla
-                resolucion = imagen.size
-                imagen = imagen.resize((350, 300))
-                metadata = imagen.getexif()
-                # convertir la imagen a un formato que pueda mostrar PySimpleGUI
-                tk_img = ImageTk.PhotoImage(imagen)
-                window["-IMAGE-"].update(data=tk_img)
-
-                #Crear función tostring que me devuelva todos los datos de la imagen
-                #window["-DESCRIPCION-"].update(csv_tostring(ruta_imagen))
-
-                window["-DESCRIPCION-"].update("Esta es una descripción")
-
+                imagen_data = etiquetar_imagenes.traer_data(values)
+                ruta_imagen = imagen_data[0]
+                #Muestro la imagen
+                datavisual_imagen = etiquetar_imagenes.mostrar_imagen(ruta_imagen)
+                window["-IMAGE-"].update(data=datavisual_imagen)
+                window["-DESCRIPCION-"].update(etiquetar_imagenes.imagen_tostring(imagen_data))  
             except PIL.UnidentifiedImageError:
                 sg.popup("¡No es una imagen!")
             except PermissionError:
                 sg.popup("¡No tienes permisos para acceder a esa carpeta!")
 
-        #Eventos    
-        if event == 'Agregar':
-            imagen_seleccionada['tag'] = values['Tag']
-            imagen_seleccionada['descripcion'] = values['Texto']
-
-        
+        #Eventos
+        window["-DESCRIPCION-"].update(etiquetar_imagenes.imagen_tostring(imagen_data))    
+        if event == 'Modificar':
+            tags = values['Tag']
+            descripcion = values['Texto']
         if event == 'Guardar':
-            #Ruta relativa a la imagen (metadata)
-            ruta = ruta_imagen 
-            #Texto descriptivo (lo saco de imagen_seleccionada)
-            descripcion = imagen_seleccionada['descripcion']
-            #lista de tags(lo saco de imagen_seleccionada)
-            tags = imagen_seleccionada['tag']
-            #resolucion (metadata)
-            # ya resuelta en resolucion = imagen.size
-            #tipo (mimetype)
-            mimetype = mimetypes.guess_type(ruta_imagen)[0]
-            #tamaño (metadata)
-            tamaño = os.path.getsize(ruta_imagen)
-            #Fecha de ultima actualización (enviar al log)
-            timestamp = datetime.timestamp(datetime.now())
-            ultima_actualizacion = datetime.fromtimestamp(timestamp)
-            #actualizar log  
-            #            
-            #ultimo perfil que actualizó ---(/!\ ver con Fran como implementar esta funcionalidad)---
-
-            #----cargar datos en el csv, buscando por ruta_imagen y reemplazando los valores de forma adecuada----
-
-            # Abro archivo.csv en modo lectura y escritura
+            imagen_data = etiquetar_imagenes.traer_data(values)
             ruta_csv = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'datos')), 'imagenes_etiquetadas.csv')
-            with open(ruta_csv, mode='r+', newline='') as file:
-                # Creo objeto lector
-                reader = csv.reader(file)
-                writer = csv.writer(file)
-                next(file, reader)
-                next(file, writer)
-                # Leo el contenido del archivo.csv y guardo la fila 
-                contenido = list(reader)
+            etiquetar_imagenes.guardar_data(ruta_csv, imagen_data)
+            #actualizar log   
 
-                # Recorro los datos
-                for fila in contenido:
-                    # Si la ruta de la imagen coincide con la ruta dada
-                    if fila[0] == ruta_imagen:
-                        # Actualizo los valores correspondientes
-                        fila[1] = descripcion
-                        fila[2] = tags
-                        fila[6] = ultima_actualizacion()
-                        fila[7] = "null" #De momento es null porque falta implementar funcionalidad
-                                         #de agregar ultimo perfil que actualizó
-                        break
-                else:
-                    # Si no se encontró una fila con la ruta dada, agreo una nueva fila (nueva imagen editada)
-                    fila_nueva = (ruta_imagen, descripcion, tags,(str(resolucion[0]) + 'x' + str(resolucion[1])), 
-                                  mimetype, (round(tamaño / (1024*1024), 2)), ultima_actualizacion, "null")
-                    #escribo la nueva fila
-                    writer.writerow(fila_nueva)
+
 
 window.close()
 
