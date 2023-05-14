a65f213aca53362ae5fefc23c9769bbf3cac75aa
Gabriela Bamba Baldivieso <gabrielabamba123@gmail.com>
2023-05-14 19:53:42 -0300

    modifico la funcion para registrar los logs

--- funcionalidad/registrar_log.py
+++ funcionalidad/registrar_log.py
@@ -1,21 +1,28 @@
 import csv
 import os
+import sys
 import datetime
-import PySimpleGUI as sg
-
-ruta = os.path.realpath(os.path.realpath("."))
-ruta_archivo = os.path.join(ruta, "logs","logs.csv")
-
+ruta = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'logs'))
+ruta_archivo = os.path.join(ruta, "logs.csv")
 
 def registrar_interaccion(nick, operacion):
     
      '''Esta función agrega un registro al archivo logs.csv'''
+     
      timestamp = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
      log = [timestamp, nick, operacion]
+
+     #para verificar si el archivo existe o no.
+     archivo_existe = os.path.isfile(ruta_archivo)
+
      try:
          with open(ruta_archivo, 'a', newline='', encoding='UTF-8') as archivo_csv:
              writer_csv = csv.writer(archivo_csv)
+             #si aun no existe, escribe la primer fila, la cual contiene los encabezados
+             if not archivo_existe:
+                 writer_csv.writerow(["Fecha y Hora", "Nick", "Operación"])   
              writer_csv.writerow(log)
-     except FileNotFoundError:
-         print("Archivo no encontrado")
+     except (FileNotFoundError,PermissionError):
+         pass
+
 
