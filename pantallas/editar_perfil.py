78b9501611ec0393a5fa2a1d1c86ec0327c9f7d8
Francisco Garrubba <franciscogarrubba@gmail.com>
2023-05-13 22:56:05 -0300

    agrego funciones a editar perfil

--- pantallas/editar_perfil.py
+++ pantallas/editar_perfil.py
@@ -57,18 +57,38 @@ layout = [
     ]
 ]
 
-def modifcar_perfil(perfil):
-    pass
+def mostrar_perfil(perfil_actual):
+    """ Guardo los valores del perfil"""
 
+    perfil = {
+        "Nombre": perfil_actual["-NOMBRE-"],
+        "Edad": perfil_actual["-EDAD-"],
+        "Genero": perfil_actual["-GENERO-"],
+        "Especificar genero": perfil_actual["-ESPECIFICAR_GENERO-"],
+        "Avatar": perfil_actual["-BROWSE-"],
+    }
+    return perfil
 
+def modificar_perfil(perfil_actual, cambios_perfil):
+    cambios_perfil = {
+        perfil_actual['-NOMBRE-']: cambios_perfil['-NOMBRE-'],
+        perfil_actual['-EDAD-']: cambios_perfil['-EDAD-'],
+        perfil_actual['-GENERO-']: cambios_perfil['-GENERO-'],
+        perfil_actual['-ESPECIFICAR_GENERO-']: cambios_perfil['-ESPECIFICAR_GENERO-'],
+        perfil_actual['-BROWSE-']: os.path.basename(cambios_perfil['-BROWSE-']),
+    }
+    return cambios_perfil
 
 window = sg.Window("Editar perfil", layout)
 
 while True:
     event, values = window.read()
 
+    mostrar_perfil(perfil_actual)
+
     if event == "-VOLVER-" or event == sg.WIN_CLOSED:
         break
+
 
     elif event == "-BROWSE-":
         filename = values["-BROWSE-"]
@@ -79,9 +99,10 @@ while True:
 
     elif event == "-GUARDAR-":
         
-        perfil_modificado = modifcar_perfil(perfil)
+        perfil_modificado = modifcar_perfil(perfil_actual,values)
+
         with open(ruta_archivo, "w") as archivo:
-            json.dump(values, archivo)
+            json.dump(perfil_modificado, archivo, indent=4)
             print("Se modifico el perfil")
 
 window.close()
