a8ebdc2de12005a530f06173016b24c3fd848d70
Gabriela Bamba Baldivieso <gabrielabamba123@gmail.com>
2023-05-26 21:27:38 -0300

    agrego las nuevas columnas al archivo de logs y modifico el formato de fecha y hora

--- funcionalidad/registrar_log.py
+++ funcionalidad/registrar_log.py
@@ -1,16 +1,16 @@
 import csv
 import os
 import sys
-import datetime
-ruta = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'logs'))
-ruta_archivo = os.path.join(ruta, "logs.csv")
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from datetime import datetime
+from rutas import archivo_logs_csv as ruta_archivo
 
-def registrar_interaccion(nick, operacion):
+def registrar_interaccion(nick, operacion,valores=None,textos=None):
     
      '''Esta función agrega un registro al archivo logs.csv'''
      
-     timestamp = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-     log = [timestamp, nick, operacion]
+     timestamp = datetime.timestamp(datetime.now())
+     log = [timestamp, nick, operacion,valores,textos]
 
      #para verificar si el archivo existe o no.
      archivo_existe = os.path.isfile(ruta_archivo)
@@ -20,7 +20,7 @@ def registrar_interaccion(nick, operacion):
              writer_csv = csv.writer(archivo_csv)
              #si aun no existe, escribe la primer fila, la cual contiene los encabezados
              if not archivo_existe:
-                 writer_csv.writerow(["Fecha y Hora", "Nick", "Operación"])   
+                 writer_csv.writerow(["Fecha y Hora", "Nick", "Operación","Valores", "Textos"])   
              writer_csv.writerow(log)
      except (FileNotFoundError,PermissionError):
          pass
