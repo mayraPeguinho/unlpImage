da7bf1fadec7a750dbf4a31f69fcacbe9b93ed4b
Gabriela Bamba Baldivieso <gabrielabamba123@gmail.com>
2023-05-30 22:23:04 -0300

    modifico pantalla generador de collage

--- pantallas/generador_collage.py
+++ pantallas/generador_collage.py
@@ -1,30 +1,43 @@
 import PySimpleGUI as sg
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from PIL import Image, ImageTk
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+#from rutas import archivo_imagenes_etiquetadas_csv as ruta_archivo
+from rutas import ruta_repositorio_imagenes as ruta_archivo_def
 from funcionalidad.verificar_input import falta_completar_campos
+def layout(cant_imagenes):
+    imagenes = [imag for imag in os.listdir(ruta_archivo_def) if imag.endswith((".jpg", ".jpeg", ".png"))]
+    column1 = [
+        [sg.Text("Seleccionar imagenes")],
+    ]
+    column1.extend([[sg.Combo(imagenes, key=f"-IMAGEN-{i+1}-")] for i in range(cant_imagenes)])
+    column1.append([sg.Button("Actualizar", key="-ACTUALIZAR-")])
 
-def generar_collage():
-     '''Esta funcion define una ventana 
-       para generar collages a partir de imágenes y plantillas seleccionadas por el usuario. '''
-     layout = [
-    [
-        sg.Text("Generar Collage", font=("Helvetica", 20), justification="left"),
-        sg.Column(
-            [[sg.Button("Volver ➡", key="-VOLVER-")]],
-            expand_x=True,
-            element_justification="right"
-        ),
-     ],
-     [sg.Text("Seleccionar imágenes")],
-    [sg.Input(), sg.FilesBrowse("Seleccionar Imagenes",key="-FILES-",file_types=(("Image Files", "*.png;*.jpg;*.jpeg;*.gif"),),files_delimiter=";",)],
-     [sg.Text("Seleccionar plantilla")],
-     [sg.Combo(["Plantilla 1", "Plantilla 2", "Plantilla 3", "Plantilla 4", "Plantilla 5"], key="Listar Plantillas")],
-     [sg.Column([[sg.Button("Generar Collage", key="-GENERAR COLLAGE-")]], expand_x=True, element_justification="right")],
-     ]
+    column2 = [
+         [sg.Image(key="-IMAGEN-", size=(400, 400))],
+         [sg.Column(
+         [[sg.Button("Guardar", key="-GUARDAR-")]],
+                     expand_x=True,
+                     element_justification="right"
+                   )
+         ],
+    ]
 
-     window = sg.Window('Generador de collage', layout, margins=(60, 80), finalize=True, resizable=True)
-
+    return [
+        [   [sg.Text("Generar Collage", font=("Helvetica", 20), justification="left"),
+             sg.Column( [[sg.Button("Volver", key="-VOLVER-")]],
+                          expand_x=True,
+                          element_justification="right"
+                      )
+           ],
+        
+            sg.Column(column1,vertical_alignment="center"),
+            sg.Column(column2,vertical_alignment="center"),
+        ]
+    ]
+def generar_collage(cant_imagenes):
+     
+     window = sg.Window("Generador de Collages", layout(cant_imagenes),margins=(60,30))
      while True:
          evento, valores = window.read()
          if evento == "-VOLVER-":
@@ -32,7 +45,7 @@ def generar_collage():
              break
          elif evento == sg.WIN_CLOSED:
              sys.exit()
-         elif evento == "-GENERAR COLLAGE-":
+         elif evento == "-GUARDAR-":
              if falta_completar_campos(valores):
                  sg.popup("Falta completar los campos necesarios")
                    
@@ -40,10 +53,14 @@ def generar_collage():
                  #funcion para crear un collage
                  sg.popup("Se genero un nuevo collage!")
                  break
+         elif evento== "-ACTUALIZAR-":
+             pass
+     window.close()
 
 
 if __name__ =="__main__":
-     generar_collage()
+     #Le paso la cantidad de imagenes que tendrá el collage
+     generar_collage(6)
 
 
     
