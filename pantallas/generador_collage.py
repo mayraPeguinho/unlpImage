e0c9fb72d604714d573a6a7b7f5d305594283a0e
Gabriela Bamba Baldivieso <gabrielabamba123@gmail.com>
2023-06-03 20:55:40 -0300

    modifico la pantalla generar collage

--- pantallas/generador_collage.py
+++ pantallas/generador_collage.py
@@ -1,16 +1,23 @@
 import PySimpleGUI as sg
 from PIL import Image, ImageTk
-import sys, os
+import sys
+import os
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-#from rutas import archivo_imagenes_etiquetadas_csv as ruta_archivo
+
 from rutas import ruta_repositorio_imagenes as ruta_archivo_def
 from funcionalidad.verificar_input import falta_completar_campos
+from rutas import ruta_diseños_collages
+
 def layout(cant_imagenes):
-    imagenes = [imag for imag in os.listdir(ruta_archivo_def) if imag.endswith((".jpg", ".jpeg", ".png"))]
+
+    imagenes = [imagen for imagen in os.listdir(ruta_archivo_def) if imagen.endswith((".jpg", ".jpeg", ".png"))]
     column1 = [
         [sg.Text("Seleccionar imagenes")],
     ]
     column1.extend([[sg.Combo(imagenes, key=f"-IMAGEN-{i+1}-")] for i in range(cant_imagenes)])
+    column1.append([sg.Text("Título")])
+    column1.append([sg.Input(key="-TÍTULO-",size=(35))])
     column1.append([sg.Button("Actualizar", key="-ACTUALIZAR-")])
 
     column2 = [
@@ -35,9 +42,22 @@ def layout(cant_imagenes):
             sg.Column(column2,vertical_alignment="center"),
         ]
     ]
-def generar_collage(cant_imagenes):
+
+
+def generar_collage(diseño,cant_imagenes):
      
-     window = sg.Window("Generador de Collages", layout(cant_imagenes),margins=(60,30))
+     window = sg.Window("Generador de Collages", layout(cant_imagenes),margins=(60,30),element_justification="center",resizable=True)
+     window.finalize()
+     
+     #muestro el diseño del collage elegido
+     path_collage=os.path.join(ruta_diseños_collages, diseño)
+     collage= Image.open(path_collage)
+     
+     imagen = ImageTk.PhotoImage(collage)
+     window["-IMAGEN-"].update(data=imagen)
+
+     
+
      while True:
          evento, valores = window.read()
          if evento == "-VOLVER-":
@@ -45,22 +65,37 @@ def generar_collage(cant_imagenes):
              break
          elif evento == sg.WIN_CLOSED:
              sys.exit()
+         elif evento == "-ACTUALIZAR-":
+                 #inicializo la lista por si seleccioné otras imagenes
+                 images=[]
+                 
+                 #obtengo la lista de imagenes con las que voy a crear el collage
+                 for i in range(cant_imagenes):
+                     image_path = os.path.join(ruta_archivo_def, valores[f"-IMAGEN-{i+1}-"])
+                     image = Image.open(image_path)
+                     images.append(image)
+
+                 #muestro el collage altualizado
+                 imagen_actualizada = ImageTk.PhotoImage(collage)
+                 window["-IMAGEN-"].update(data=imagen_actualizada)
+
+
          elif evento == "-GUARDAR-":
-             if falta_completar_campos(valores):
-                 sg.popup("Falta completar los campos necesarios")
-                   
-             else:
-                 #funcion para crear un collage
-                 sg.popup("Se genero un nuevo collage!")
+             if not falta_completar_campos(valores):
+                 #Guardo el collage
+                 sg.popup("El collage se generó con éxito")
                  break
-         elif evento== "-ACTUALIZAR-":
-             pass
+             else:
+                  sg.popup("Falta completar los campos necesarios")
+           
      window.close()
 
 
 if __name__ =="__main__":
-     #Le paso la cantidad de imagenes que tendrá el collage
-     generar_collage(6)
-
+     #diseño seleccionado
+     diseño = 'collage_3.png'
+     #cantidad de imagenes que tendrá el collage
+     cant_imagenes = 4
+     generar_collage(diseño, cant_imagenes)
 
     
