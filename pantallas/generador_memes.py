d4c00a288ccbd04ffeb5bbc80ad2b4535c52f84b
Francisco Garrubba <franciscogarrubba@gmail.com>
2023-06-09 17:55:26 -0300

    agrego para seleccionar fuente

--- pantallas/generador_memes.py
+++ pantallas/generador_memes.py
@@ -7,36 +7,51 @@ from funcionalidad.verificar_input import falta_completar_campos
 
 
 def generar_meme():
-     '''Esta funcion define una ventana 
-     para generar memes a partir de una imágen y un seleccionados por el usuario. '''
-     layout = [
-             [ sg.Text("Generar Meme", font=("Helvetica", 20), justification="left"),
-             sg.Column([[sg.Button("Volver ➡", key="-VOLVER-")]],expand_x=True,element_justification="right"),],
-             [sg.Text('Seleccionar imagen:')],
-             [sg.Input(), sg.FileBrowse("Seleccionar Imagen",file_types=(("Image Files", "*.png;*.jpg;*.jpeg;*.gif"),))],
-             [sg.Text('Introducir el texto:')],
-             [sg.InputText()],
-             [sg.Column([[sg.Button("Generar", key="-GENERAR-")]], expand_x=True, element_justification="right")],
 
-            
-           ]
-     window = sg.Window('Generador de memes',layout, margins=(60, 80), finalize=True, resizable=True)
+    fuentes = sg.Text.fonts_installed_list() 
+    
+    '''Esta funcion define una ventana 
+    para generar memes a partir de una imágen y un seleccionados por el usuario. '''
+    
+    columna_izquierda = [[sg.Text('Seleccionar fuente:')],
+        [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key='-FUENTE-')]
+    ]
 
-     while True:
-         evento, valores = window.Read()
-         if evento == "-GENERAR-" :
-             if not falta_completar_campos(valores) :  
-                 #genero el meme
-                 sg.popup("Se generó un meme!")
-                 break
-             else:
-                 sg.popup("No se completaron los campos necesarios")
-         elif evento == "-VOLVER-":
-             window.close()
-             break
-         elif evento== sg.WIN_CLOSED:
-             sys.exit()
+
+    columna_derecha = [
+         [sg.Image(key="-IMAGEN-", size=(200, 200))],
+         [sg.Column(
+         [[sg.Button("Guardar", key="-GUARDAR-")]],
+                     expand_x=True,
+                     element_justification="right"
+                   )
+         ],
+    ]
+
+    layout = [
+        [
+            sg.Column(columna_izquierda, element_justification="c"),
+            sg.VSeperator(),
+            sg.Column(columna_derecha, element_justification="c"),
+        ]
+    ]
+    window = sg.Window('Generador de memes',layout, margins=(60, 80), finalize=True, resizable=True)
+
+    while True:
+        evento, values = window.Read()
+        print(values['-FUENTE-'])
+        if evento == "-VOLVER-":
+            window.close()
+            break
+        elif evento== sg.WIN_CLOSED:
+            sys.exit()
+        elif evento == "-GENERAR-" :
+            if not falta_completar_campos(values) :  
+                #genero el meme
+                sg.popup("Se generó un meme!")
+                break
+            else:
+                sg.popup("No se completaron los campos necesarios")
 
 if __name__ =="__main__":
-     generar_meme()
-  
+    generar_meme()
