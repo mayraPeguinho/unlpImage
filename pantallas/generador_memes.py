ffc96cddb13c3a275b2734095fb1353bbd4bb69b
Francisco Garrubba <franciscogarrubba@gmail.com>
2023-06-11 19:59:56 -0300

    Agrego diferentes opciones de menu segun la cantidad de boxes que tiene

--- pantallas/generador_memes.py
+++ pantallas/generador_memes.py
@@ -14,36 +14,61 @@ from funcionalidad.crear_meme import *
 
 ruta_avatares= os.path.join(r.ruta_imagenes_perfil,'hombre_traje.png')
 
-def generar_meme():
 
-    fuentes = sg.Text.fonts_installed_list() 
+fuentes = sg.Text.fonts_installed_list() 
     
-    '''Esta funcion define una ventana 
-    para generar memes a partir de una imágen y un seleccionados por el usuario. '''
+'''Esta funcion define una ventana 
+para generar memes a partir de una imágen y un seleccionados por el usuario. '''
+def definir_layout():   
+
+    cant_cajas=1
     
-    columna_izquierda = [[sg.Text('Seleccionar fuente:')],
-        [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key='-FUENTE-')],
-        [sg.Button('Actualizar', key=('-ACTUALIZAR-'))]
-    ]
+
+    if (cant_cajas == 1):
+        columna_izquierda = [[sg.Text('Seleccionar fuente:')],
+            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key='-FUENTE-')],
+            [sg.Input('Texto 1:', size=(25,5))],
+            [sg.Button('Actualizar', key=('-ACTUALIZAR-'))]
+        ]
+    elif (cant_cajas == 2):
+        columna_izquierda = [[sg.Text('Seleccionar fuente:')],
+            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key='-FUENTE-')],
+            [sg.Input('Texto 1:', size=(20,20))],[sg.Input('Texto 2:', size=(20,20))],
+            [sg.Button('Actualizar', key=('-ACTUALIZAR-'))]
+        ]
+    elif (cant_cajas == 3):
+        columna_izquierda = [[sg.Text('Seleccionar fuente:')],
+            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key='-FUENTE-')],
+            [sg.Input('Texto 1:', size=(20,20))],[sg.Input('Texto 2:', size=(20,20))],[sg.Input('Texto 3:', size=(20,20))],
+            [sg.Button('Actualizar', key=('-ACTUALIZAR-'))]
+        ]
 
 
     columna_derecha = [
-         [sg.Image(key="-IMAGEN-", size=(200, 200))],
-         [sg.Column(
-         [[sg.Button("Guardar", key="-GUARDAR-")]],
-                     expand_x=True,
-                     element_justification="right"
-                   )
-         ],
+        [sg.Image(key="-IMAGEN-", size=(200, 200))],
+        [sg.Column(
+        [[sg.Button("Guardar", key="-GUARDAR-")]],
+                    expand_x=True,
+                    element_justification="right"
+                    )
+        ],
     ]
 
     layout = [
-        [
-            sg.Column(columna_izquierda, element_justification="c"),
-            sg.VSeperator(),
-            sg.Column(columna_derecha, element_justification="c"),
+            [
+                sg.Column(columna_izquierda, element_justification="c"),
+                sg.VSeperator(),
+                sg.Column(columna_derecha, element_justification="c"),
+            ]
         ]
-    ]
+
+    return layout
+
+
+def generar_meme():
+
+    layout = definir_layout()
+
     window = sg.Window('Generador de memes',layout, margins=(60, 80), finalize=True, resizable=True)
 
     """Muestro el meme que se me pasa por parametro"""
@@ -65,7 +90,7 @@ def generar_meme():
             sys.exit()
         
         elif event == '-ACTUALIZAR-':
-            meme_actual = actualizar_datos(imagen_meme)
+            meme_actual = actualizar_datos(imagen_meme,values['-FUENTE-'],values['-TEXTO-'])
     
         elif event == "-GENERAR-" :
             if not falta_completar_campos(values) :  
