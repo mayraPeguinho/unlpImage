68502f59550c6926bd8d3399520e34e176687d3b
EZEQUIEL PALLINI <ezequielpallini2@gmail.com>
2023-06-11 20:00:18 -0300

    Corregido código extra

--- pantallas/seleccion_template.py
+++ pantallas/seleccion_template.py
@@ -74,8 +74,6 @@ def pantalla_seleccionartemplate(usuario):
             data = json.load(archivo)
         rutas_imagenes = [item['image'] for item in data]
 
-        cuadros_de_texto = [item['text_boxes'] for item in data if (item['image'] == imagen_seleccionada)]
-        cantidad_cuadros = cuadros_de_texto.__len__()
 
         for e in rutas_imagenes:
             print(e)            
@@ -85,7 +83,7 @@ def pantalla_seleccionartemplate(usuario):
             break
         elif event == sg.WIN_CLOSED:
             sys.exit()           
-        else
+        else:
             if event == '-TREE-':
                 
                 #Chequear que se pueda abrir y tratar la imagen
