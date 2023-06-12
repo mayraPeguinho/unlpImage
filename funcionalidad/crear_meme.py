515fd1c6db6f3c1a1623c2e824e78f8e0da2819d
Francisco Garrubba <franciscogarrubba@gmail.com>
2023-06-12 14:57:51 -0300

    Agrego keys a las diferentes cajas de textos, y modifico un poco funcionalidad

--- funcionalidad/crear_meme.py
+++ funcionalidad/crear_meme.py
@@ -3,12 +3,32 @@ import PIL.ImageTk
 import PIL.ImageOps
 import PIL.ImageDraw
 
-def actualizar_datos(meme,fuente,texto):
+def actualizar_datos(meme,values,fuente):
     """Se actualiza lo escrito en el meme"""
 
     copia = meme.copy()
     draw = PIL.ImageDraw.Draw(copia)
+    if (values['TEXTO_1'] != ''):
+        draw.text([top_left_x, top_left_y], values['TEXTO_1'], font=fuente)
+    
+    if (values['TEXTO_2'] != ''):
+        draw.text([top_left_x, top_left_y], values['TEXTO_2'], font=fuente)
+    
+    if (values['TEXTO_3'] != ''):
+        draw.text([top_left_x, top_left_y], values['TEXTO_3'], font=fuente)
 
-    draw.text([top_left_x, top_left_y], texto, font=fuente)
+    if (values['TEXTO_4'] != ''):
+        draw.text([top_left_x, top_left_y], values['TEXTO_4'], font=fuente)
+
+    if (values['TEXTO_5'] != ''):
+        draw.text([top_left_x, top_left_y], values['TEXTO_5'], font=fuente)
 
     return copia
+
+def recorrer_archivo(archivo):
+    """Cuento en el archivo json la cantidad de text_boxes que tiene"""
+
+    cuadros_de_texto = [item['text_boxes'] for item in data if (item['image'] == imagen_seleccionada)]
+    cantidad_cuadros = cuadros_de_texto.__len__()
+
+    return cantidad_cuadros
--- pantallas/generador_memes.py
+++ pantallas/generador_memes.py
@@ -1,104 +1,142 @@
 import PySimpleGUI as sg
 import sys
 import os
+import json
 import PIL.Image
 import PIL.ImageTk
 import PIL.ImageOps
 import PIL.ImageDraw
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from funcionalidad.verificar_input import falta_completar_campos
 import rutas as r
 from funcionalidad.crear_meme import *
 
+ruta_avatares = os.path.join(r.ruta_imagenes_perfil, "hombre_traje.png")
 
-ruta_avatares= os.path.join(r.ruta_imagenes_perfil,'hombre_traje.png')
 
+fuentes = sg.Text.fonts_installed_list()
 
-fuentes = sg.Text.fonts_installed_list() 
-    
-'''Esta funcion define una ventana 
-para generar memes a partir de una imágen y un seleccionados por el usuario. '''
-def definir_layout():   
-
-    cant_cajas=1
-    
-
-    if (cant_cajas == 1):
-        columna_izquierda = [[sg.Text('Seleccionar fuente:')],
-            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key='-FUENTE-')],
-            [sg.Input('Texto 1:', size=(25,5))],
-            [sg.Button('Actualizar', key=('-ACTUALIZAR-'))]
+def definir_layout(cant_cajas):
+    """Se arma de que manera puede ser la interfaz dependiendo la cantidad
+    de cajas de texto se tiene"""
+    cant_cajas=5
+    if cant_cajas == 1:
+        columna_izquierda = [
+            [sg.Text("Seleccionar fuente:")],
+            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key="-FUENTE-")],
+            [sg.Text('Texto 1:')],[sg.Input(key = '-TEXTO_1-')],
+            [sg.Button("Actualizar", key=("-ACTUALIZAR-"))],
         ]
-    elif (cant_cajas == 2):
-        columna_izquierda = [[sg.Text('Seleccionar fuente:')],
-            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key='-FUENTE-')],
-            [sg.Input('Texto 1:', size=(20,20))],[sg.Input('Texto 2:', size=(20,20))],
-            [sg.Button('Actualizar', key=('-ACTUALIZAR-'))]
+    elif cant_cajas == 2:
+        columna_izquierda = [
+            [sg.Text("Seleccionar fuente:")],
+            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key="-FUENTE-")],
+            [sg.Text('Texto 1:')],[sg.Input(key = '-TEXTO_1-')],
+            [sg.Text('Texto 2:')],[sg.Input(key = '-TEXTO_2-')],
+            [sg.Button("Actualizar", key=("-ACTUALIZAR-"))],
         ]
-    elif (cant_cajas == 3):
-        columna_izquierda = [[sg.Text('Seleccionar fuente:')],
-            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key='-FUENTE-')],
-            [sg.Input('Texto 1:', size=(20,20))],[sg.Input('Texto 2:', size=(20,20))],[sg.Input('Texto 3:', size=(20,20))],
-            [sg.Button('Actualizar', key=('-ACTUALIZAR-'))]
+    elif cant_cajas == 3:
+        columna_izquierda = [
+            [sg.Text("Seleccionar fuente:")],
+            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key="-FUENTE-")],
+            [sg.Text('Texto 1:')],[sg.Input(key = '-TEXTO_1-')],
+            [sg.Text('Texto 2:')],[sg.Input(key = '-TEXTO_2-')],
+            [sg.Text('Texto 3:')],[sg.Input(key = '-TEXTO_3-')],
+            [sg.Button("Actualizar", key=("-ACTUALIZAR-"))],
         ]
-
+    elif cant_cajas == 4:
+        columna_izquierda = [
+            [sg.Text("Seleccionar fuente:")],
+            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key="-FUENTE-")],
+            [sg.Text('Texto 1:')],[sg.Input(key = '-TEXTO_1-')],
+            [sg.Text('Texto 2:')],[sg.Input(key = '-TEXTO_2-')],
+            [sg.Text('Texto 3:')],[sg.Input(key = '-TEXTO_3-')],
+            [sg.Text('Texto 4:')],[sg.Input(key = '-TEXTO_4-')],
+            [sg.Button("Actualizar", key=("-ACTUALIZAR-"))],
+        ]
+    elif cant_cajas == 5:
+        columna_izquierda = [
+            [sg.Text("Seleccionar fuente:")],
+            [sg.Listbox(fuentes, size=(20, 5), change_submits=True, key="-FUENTE-")],
+            [sg.Text('Texto 1:')],[sg.Input(key = '-TEXTO_1-')],
+            [sg.Text('Texto 2:')],[sg.Input(key = '-TEXTO_2-')],
+            [sg.Text('Texto 3:')],[sg.Input(key = '-TEXTO_3-')],
+            [sg.Text('Texto 4:')],[sg.Input(key = '-TEXTO_4-')],
+            [sg.Text('Texto 5:')],[sg.Input(key = '-TEXTO_5-')],
+            [sg.Button("Actualizar", key=("-ACTUALIZAR-"))],
+        ]
 
     columna_derecha = [
         [sg.Image(key="-IMAGEN-", size=(200, 200))],
-        [sg.Column(
-        [[sg.Button("Guardar", key="-GUARDAR-")]],
-                    expand_x=True,
-                    element_justification="right"
-                    )
+        [
+            sg.Column(
+                [[sg.Button("Guardar", key="-GUARDAR-")]],
+                expand_x=True,
+                element_justification="right",
+            )
         ],
     ]
 
     layout = [
-            [
-                sg.Column(columna_izquierda, element_justification="c"),
-                sg.VSeperator(),
-                sg.Column(columna_derecha, element_justification="c"),
-            ]
+        [
+            sg.Column(columna_izquierda, element_justification="c"),
+            sg.VSeperator(),
+            sg.Column(columna_derecha, element_justification="c"),
         ]
+    ]
 
     return layout
 
 
 def generar_meme():
+    
+    #try:
+    #    with open(ruta_archivo, "r", encoding="UTF-8") as archivo:
+    #        archivo_memes = json.load(archivo)
+    #except (FileNotFoundError, json.JSONDecodeError):
+    #    sg.popup_error("""El archivo 'template.json' no se encontró, la aplicacion no puede continuar y se cerrará el programa.""")
+    #    sys.exit()
+    #except PermissionError:
+    #    sg.popup_error("""No se cuentan con los permisos para acceder al archivo 'template.json', por lo que la aplicacion no puede continuar, se cerrará el programa.""")
+    #s    sys.exit()
+    
+    #cant_cajas = recorrer_archivo(archivo_memes)
+    cant_cajas=5
+    layout = definir_layout(cant_cajas)
 
-    layout = definir_layout()
-
-    window = sg.Window('Generador de memes',layout, margins=(60, 80), finalize=True, resizable=True)
+    window = sg.Window(
+        "Generador de memes", layout, margins=(60, 80), finalize=True, resizable=True
+    )
 
     """Muestro el meme que se me pasa por parametro"""
     cargar_meme = PIL.Image.open(ruta_avatares)
 
     imagen_meme = PIL.ImageTk.PhotoImage(cargar_meme)
-    window['-IMAGEN-'].update(data=imagen_meme)
-
+    window["-IMAGEN-"].update(data=imagen_meme)
 
     while True:
         event, values = window.Read()
-        print(values['-FUENTE-'])
+        print(values["-FUENTE-"])
         window
         if event == "-VOLVER-":
             window.close()
             break
 
-        elif event== sg.WIN_CLOSED:
+        elif event == sg.WIN_CLOSED:
             sys.exit()
-        
-        elif event == '-ACTUALIZAR-':
-            meme_actual = actualizar_datos(imagen_meme,values['-FUENTE-'],values['-TEXTO-'])
-    
-        elif event == "-GENERAR-" :
-            if not falta_completar_campos(values) :  
-                #genero el meme
+
+        elif event == "-ACTUALIZAR-":
+            meme_actual = actualizar_datos(imagen_meme,values,values['-FUENTE-'])
+
+        elif event == "-GENERAR-":
+            if not falta_completar_campos(values):
+                # genero el meme
                 sg.popup("Se generó un meme!")
                 break
             else:
                 sg.popup("No se completaron los campos necesarios")
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     generar_meme()
